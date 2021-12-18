cfad418e68ed1ff37bdcad1943708b2e8b05ea61
Aramanch <90024366+Aramanch@users.noreply.github.com>
2021-12-18 17:18:05 +0300

    Update sem_work_27_11.js

--- Homework 11-12/sem_work_27_11.js
+++ Homework 11-12/sem_work_27_11.js
@@ -1,4 +1,4 @@
-/*
+
 
 console.log('Задания 1-5')
 
@@ -46,15 +46,26 @@ console.log('Задание 5');
 console.log(city1.getCity(), city2.getCity());
 
 
-*/
 
 
 
+console.log('Задание 6')
+let obj = {};
+obj.method3 = function () {
+    return "метод3";
+};
+obj.method2 = function () {
+    return this;
+};
+obj.method1 = function () {
+    return this;
+};
+console.log(obj.method1().method2().method3());
 
 
 // Решено
 
-/*
+
 
 console.log('Задание 7')
 
@@ -66,12 +77,12 @@ console.log(d1)
 console.log(d1[6])
 console.log(d1[7])
 
-*/
 
 
 
 
-/*
+
+
 
 console.log('Задание 8')
 
@@ -83,12 +94,12 @@ for (let i=0; i < d2.length;i++){
 }
 
 console.log(sum2)
-*/
 
 
 
 
-/*
+
+
 
 
 console.log('Задание 9')
@@ -103,10 +114,10 @@ for (let i=0; i < d3.length;i++){
 
 console.log(sum3)
 
-*/
 
 
-/*
+
+
 console.log('Задание 10')
 
 let d4 = [45,78,10,3];
@@ -116,10 +127,10 @@ function my(a, b) {
 
 console.log(d4.sort(my));
 
-*/
 
 
-/*
+
+
 
 console.log('Задание 11')
 
@@ -137,9 +148,9 @@ for (let i=0; i < 3;i++){
 
 console.log(d5)
 
-*/
 
-/*
+
+
 
 console.log('Задание 12');
 
@@ -168,7 +179,6 @@ console.log(new Vector(1,2).plus(new Vector(2,3)));
 console.log(new Vector(1,2).minus(new Vector(2,3)));
 console.log(new Vector(3,4).length());
 
-*/
 
 
 
@@ -176,22 +186,52 @@ console.log(new Vector(3,4).length());
 
 
 
+console.log('Задание 13');
+
+function TextCell(text){
+    this.text = text;
+}
+
+function StretchCell(inner, width, height){
+    this.text = inner.text;
+    this.width = width;
+    this.height = height;
+
+    this.minWidth = function () {
+        return Math.max(
+            this.text.split('\n').sort(
+                function(a,b){
+                    return b.length - a.length})[0]
+                .length,
+            this.width)
+    }
+
+    this.minHeight = function () {
+        return Math.max(this.text.split('\n').length, this.height)
+    }
+
+    this.draw = function(width, height){
+        text_joined = this.text.split('\n').join('');
+        text_joined += " ".repeat(width*height - text_joined.length);
+        cell = [];
+        for (let i=0; i<height; i++){
+            s = '';
+            for (let j=0; j<width; j++){
+                s += text_joined[i*width + j];
+            }
+            cell.push(s);
+        }
+        return cell;
+    }
+}
+
+var sc = new StretchCell(new TextCell("ab\nde\nevf"), 1, 2);
+console.log(sc.minWidth());
+console.log(sc.minHeight());
+console.log(sc.draw(7,3));
 
 
 
-
-
-
-
-
-
-
-
-
-
-/*
-
-# Решено
 console.log('Задание 15')
 
 function Card(from, to){
@@ -206,10 +246,10 @@ function Card(from, to){
 
 let c1 = new Card('Екатеринбург', 'Москва')
 c1.show();
-*/
 
 
-/*
+
+
 console.log('Задание 16')
 
 
@@ -250,11 +290,12 @@ let hum10 = new Human("Женя", 25, 175)
 
 
 console.log(hum1.getInfo());
-*/
 
 
 
-// Не решен
+
+
+
 console.log('Задание 17')
 let humans = [
     ['Коля', 23, 180],
@@ -307,11 +348,22 @@ console.log(sortByHeight(humans));
 
 
 
+console.log('Задание 18')
+let dt1 = new Date(2045, 0, 1, 0,0);
+console.log(dt1);
 
 
 
-/*
-# Решено
+console.log('Задание 19')
+dt2 = Date.now() / 1000
+console.log(dt2)
+
+
+
+
+
+
+
 
 
 console.log('Задание 20')
@@ -334,9 +386,88 @@ function getDays(year,month){
 }
 
 console.log(getDays(2008,'февраль'))
-*/
 
 
+
+console.log('Задание 22')
+
+Number.example.isOdd = function(dig) { if (dig % 2 == 1) return true; else return false;}
+
+let a1 = 7;
+let a2 = a1.isOdd(a1)
+console.log(a2)
+
+
+
+
+
+
+
+
+console.log("Задание 23");
+
+
+function Ladder(step){
+    this.step = step,
+    this.add = function(i){
+        this.step += i;
+        return this;
+    },
+
+    this.mins = function(i){
+        this.step -= i;
+        return this;
+    },
+
+    this.pls = function(i){
+        this.step *= i;
+        return this;
+    },
+
+    this.div = function(i){
+        this.step /= i;
+        return this;
+    }
+    
+}
+
+let otv = new Ladder(7).add(2).mins(1).pls(3).div(8);
+console.log(otv.step);
+
+
+
+
+
+
+console.log('Задание 24');
+
+function Unit(x, y){
+    this.x = x;
+    this.y = y;
+}
+
+Unit.example = {
+	
+    get X() {return this.x;},
+    get Y() {return this.y;},
+    set X(value) {this.x=value;},
+    set Y(value) {this.y=value;}
+}
+
+function Fighter(power){
+    this.power = power;
+}
+
+Fighter.example = Unit.example;
+Fighter.example += {
+    get power() {return power;},
+    set power(value) {this.power = value;}
+}
+
+f = new Fighter(8);
+f.X = 7;
+f.Y = 8;
+console.log(f.Y);
 
 
 
