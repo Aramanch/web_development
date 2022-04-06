477b40810f59be2c4c1aa6c534fb467146535617
Aramanch <90024366+Aramanch@users.noreply.github.com>
2022-04-06 23:05:12 +0300

    Update index.jsx

--- SEMESTR 2/HW5/index.jsx
+++ SEMESTR 2/HW5/index.jsx
@@ -1,66 +1,102 @@
-class TodoApp extends React.Component {
+// class TodoApp extends React.Component {
+//     constructor(props) {
+//       super(props);
+//       this.state = { items: [], text: '' };
+//       this.handleChange = this.handleChange.bind(this);
+//       this.handleSubmit = this.handleSubmit.bind(this);
+//     }
+  
+//     render() {
+//       return (
+//         <div>
+//           <h3>TODO</h3>
+//           <TodoList items={this.state.items} />
+//           <form onSubmit={this.handleSubmit}>
+//             <label htmlFor="new-todo">
+//               What needs to be done?
+//             </label>
+//             <input
+//               id="new-todo"
+//               onChange={this.handleChange}
+//               value={this.state.text}
+//             />
+//             <button>
+//               Add #{this.state.items.length + 1}
+//             </button>
+//           </form>
+//         </div>
+//       );
+//     }
+  
+//     handleChange(e) {
+//       this.setState({ text: e.target.value });
+//     }
+  
+//     handleSubmit(e) {
+//       e.preventDefault();
+//       if (this.state.text.length === 0) {
+//         return;
+//       }
+//       const newItem = {
+//         text: this.state.text,
+//         id: Date.now()
+//       };
+//       this.setState(state => ({
+//         items: state.items.concat(newItem),
+//         text: ''
+//       }));
+//     }
+//   }
+  
+//   class TodoList extends React.Component {
+//     render() {
+//       return (
+//         <ul>
+//           {this.props.items.map(item => (
+//             <li key={item.id}>{item.text}</li>
+//           ))}
+//         </ul>
+//       );
+//     }
+//   }
+  
+//   ReactDOM.render(
+//     <TodoApp></TodoApp>,
+//     document.getElementById("app")
+// )
+
+class Timer extends React.Component {
     constructor(props) {
       super(props);
-      this.state = { items: [], text: '' };
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
+      this.state = { seconds: 0 };
+    }
+  
+    tick() {
+      this.setState(state => ({
+        seconds: state.seconds + 1
+      }));
+    }
+  
+    componentDidMount() {
+      this.interval = setInterval(() => this.tick(), 1000);
+    }
+  
+    componentWillUnmount() {
+      clearInterval(this.interval);
     }
   
     render() {
       return (
         <div>
-          <h3>TODO</h3>
-          <TodoList items={this.state.items} />
-          <form onSubmit={this.handleSubmit}>
-            <label htmlFor="new-todo">
-              What needs to be done?
-            </label>
-            <input
-              id="new-todo"
-              onChange={this.handleChange}
-              value={this.state.text}
-            />
-            <button>
-              Add #{this.state.items.length + 1}
-            </button>
-          </form>
+          Seconds: {this.state.seconds}
         </div>
       );
     }
-  
-    handleChange(e) {
-      this.setState({ text: e.target.value });
-    }
-  
-    handleSubmit(e) {
-      e.preventDefault();
-      if (this.state.text.length === 0) {
-        return;
-      }
-      const newItem = {
-        text: this.state.text,
-        id: Date.now()
-      };
-      this.setState(state => ({
-        items: state.items.concat(newItem),
-        text: ''
-      }));
-    }
   }
   
-  class TodoList extends React.Component {
-    render() {
-      return (
-        <ul>
-          {this.props.items.map(item => (
-            <li key={item.id}>{item.text}</li>
-          ))}
-        </ul>
-      );
-    }
-  }
-  
-  ReactDOM.render(
-    <TodoApp></TodoApp>,
+
+
+    ReactDOM.render(
+    <Timer></Timer>,
     document.getElementById("app")
 )
